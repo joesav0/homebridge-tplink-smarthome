33cac98195af5c5827e7a37e132eb03d5b934fd7
Joe Savino <joesavino@MacBook-Air.local>
2025-09-23 18:22:19 -0700

    Update for Homebridge 2.0 compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import type { API } from 'homebridge';
-
-import { PLATFORM_NAME } from './settings';
 import TplinkSmarthomePlatform from './platform';
 
 export = (api: API): void => {
-  api.registerPlatform(PLATFORM_NAME, TplinkSmarthomePlatform);
+  api.registerPlatform(
+    'homebridge-tplink-smarthome',
+    'TplinkSmarthomePlatform',
+    TplinkSmarthomePlatform
+	);
 };
